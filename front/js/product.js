6e0a276e64d05d222c35a02cecca786fff49827a
gabtor7 <gabey.toreilles@gmail.com>
2022-10-07 14:55:17 +0200

    re-fix button disable + processLocalStorage

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,8 @@
 const searchParams = new URLSearchParams(window.location.search);
 const id = searchParams.get('id');
 let product, colorIsOk, qtyIsOk;
+let jsonCart;
+let allKanaps = [];
 
 const addBtn = document.getElementById('addToCart');
 addBtn.disabled = true;
@@ -69,12 +71,11 @@ async function createProduct() {
 
     colors.addEventListener('change', () => {
         // Vérifier la selection d'une couleur par l'utilisateur
-        if(!colors.value){
+        if(colors.value){
+            enableAddBtn();
+        } else {
             alert('Veuillez choisir une couleur de canapé');
             return;
-        } else {
-            colorIsOk = true;
-            enableAddBtn();
         }
     });
 
@@ -92,10 +93,11 @@ async function createProduct() {
         // } else {
         //     alert(kanapQty);
         // }
-
+        // Tableau contenant tous les kanap au fur et à mesure des ajouts dans le panier
+        
         let kanap = {
             _id: product._id,
-            colors: colors.value,
+            color: colors.value,
             quantity: Number(quantity.value)
         };
 
@@ -106,29 +108,86 @@ async function createProduct() {
     });
 })();
 
-function processLocalStorage(object) {
-    // Si l'objet n'existe pas dans le localStraoge, dans ce on le crée
+function processLocalStorage(kanap) {
+
+    // Ajout du kanap au tableau/panier de kanaps
+    addToTab(kanap);
+
+    // Vérification et création d'un panier dans le localStorage si il n'y en a pas, puis ajout du produit
+    addToCart('kanapCart', kanap);
+
+    // Si l'objet n'existe pas dans le localStorage, on le crée
     // A l'inverse, si l'objet existe déjà (id et couleur identiques), dans ce cas on l'ajoute
 
     // Avant ajout on vérifie l'existence d'un panier dans le localStorage
-    if (!localStorage.getItem('kanapCart')){
-        // Aucun panier trouvé, on en crée un et ajoute le kanap courant
-        localStorage.setItem('kanapCart', JSON.stringify(object));
-        console.log(kanapCart);
-    } else {
-        // Add in localStorage in the existing array
-        // Get the array from the cart and convert it to JSON format
-        let cart = JSON.stringify(localStorage.getItem('kanapCart'));
-        let jsonCart = JSON.parse(cart);
-        console.log(jsonCart.json());
+    // if (!localStorage.getItem('kanapCart')){
+    //     // Aucun panier trouvé, on en crée un et ajoute le kanap courant
+    //     localStorage.setItem('kanapCart', JSON.stringify(object));
+    //     console.log(kanapCart);
+    // } else {
+    //     // Add in localStorage in the existing array
+    //     // Get the array from the cart and convert it to JSON format
+    //     let cart = JSON.stringify(localStorage.getItem('kanapCart'));
+    //     let jsonCart = JSON.parse(cart);
+    //     console.log(jsonCart.json());
+    // }
+}
 
-        //run/loop through the cart and check if the ID we're trying to add isn't already in the cart
+/**
+ * Adds the desired kanap to a tab containing all kanaps in the cart
+ * @param {Object} kanap - the kanap to be added
+ */
+function addToTab(kanap){
+    
+    let addToTab = false;
+    jsonCart['products'].forEach(prod => {
+        if(kanap._id == prod._id){
+            if(kanap.color == prod.color){
+                // id et couleur existants (sum de quantité), pas d'ajout, modification du total du kanap existant
+                checkQtyKanap(kanap, prod);
+            } else {
+                // id existe dans le panier mais pas la couleur, on ajoute
+                addToTab = true;
+            }
+        } else {
+            // id non existant dans le panier, on ajoute
+            addToTab = true;
+        }
+    });
+    
+    if(addToTab){
+        allKanaps.push(kanap);
+        jsonCart = {
+            'products': allKanaps,
+        };
+        console.log('Produit ajouté au panier.');
+    }
+}
 
-        //it exists: check if the color is the same
-        // same color: we add to the quantity
-        //  if qty > 100 display a warning and cap the maximum to 100
-        //  else simply add the desired number to the sum
-        // not the same color: add the object {color: qty}
-        //it does not exist, add the whole thing
+/**
+ * Adds the whole tab of kanaps to the cart, usually with the new kanap
+ * @param {String} cartName - name of the cart in localStorage
+ * @param {Array} tabAllKanap - the tab containing all kanaps, will replace the current one
+ */
+function addToCart(cartName, tabAllKanap){
+    localStorage.setItem(cartName, JSON.stringify(tabAllKanap));
+    
+}
+
+
+// function addItemToCart(cartName, item){
+//     // Récupérer le tableau des produits depuis localStorage
+//     let cart = JSON.stringify(localStorage.getItem(cartName));
+//     // Convertir en JSON
+//     let jsonCart = JSON.parse(cart);
+//     // Ajouter le produit au tableau JSON (effectuer vérifications nécessaires)
+
+// }
+
+function checkQtyKanap(kanapToAdd, kanapInCart){
+    // On attribuera la somme total au kanapInCart
+    if((kanapToAdd.quantity + kanapInCart.quantity) > 100){
+        kanapInCart.quantity = 100;
+        alert('Vous ne pouvez pas avoir plus de 100 kanap. Les kanap en trop seront ignorés.')
     }
 }
